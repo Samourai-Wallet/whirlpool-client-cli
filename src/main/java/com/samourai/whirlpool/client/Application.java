dbe75fb774adc829b8ed46739b4f36bdb360ba89
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2018-09-29 19:16:47 +0200

    Protocol upgrade: add PoolInfo.mixNbConfirmed

--- src/main/java/com/samourai/whirlpool/client/Application.java
+++ src/main/java/com/samourai/whirlpool/client/Application.java
@@ -115,9 +115,9 @@ public class Application implements ApplicationRunner {
             String lineFormat = "| %15s | %6s | %15s | %22s | %12s | %15s | %13s |\n";
             StringBuilder sb = new StringBuilder();
             sb.append(String.format(lineFormat, "POOL ID", "DENOM.", "STATUS", "USERS", "ELAPSED TIME", "ANONYMITY SET", "MINER FEE"));
-            sb.append(String.format(lineFormat, "", "(btc)", "", "(registered/connected)", "", "(target/min)", "min-max (sat)"));
+            sb.append(String.format(lineFormat, "", "(btc)", "", "(confirmed/registered)", "", "(target/min)", "min-max (sat)"));
             for (Pool pool : pools.getPools()) {
-                sb.append(String.format(lineFormat, pool.getPoolId(),  satToBtc(pool.getDenomination()), pool.getMixStatus(), pool.getMixNbRegistered() + " / " + pool.getMixNbConnected(), pool.getElapsedTime()/1000 + "s", pool.getMixAnonymitySet() + " / " + pool.getMinAnonymitySet(), pool.getMinerFeeMin() + " - " + pool.getMinerFeeMax()));
+                sb.append(String.format(lineFormat, pool.getPoolId(),  satToBtc(pool.getDenomination()), pool.getMixStatus(), pool.getMixNbConfirmed() + " / " + pool.getNbRegistered(), pool.getElapsedTime()/1000 + "s", pool.getMixAnonymitySet() + " / " + pool.getMinAnonymitySet(), pool.getMinerFeeMin() + " - " + pool.getMinerFeeMax()));
             }
             log.info("\n" + sb.toString());
             log.info("Tip: use --pool argument to select a pool");
