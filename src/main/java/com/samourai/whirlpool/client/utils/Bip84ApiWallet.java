b054bc744bbd4c9483080eb0e28e18707fa04ae9
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2018-12-07 14:10:05 +0100

    initBip84: retry on fail

--- src/main/java/com/samourai/whirlpool/client/utils/Bip84ApiWallet.java
+++ src/main/java/com/samourai/whirlpool/client/utils/Bip84ApiWallet.java
@@ -4,6 +4,7 @@ import com.samourai.api.SamouraiApi;
 import com.samourai.api.beans.MultiAddrResponse;
 import com.samourai.api.beans.UnspentResponse.UnspentOutput;
 import com.samourai.wallet.hd.HD_Wallet;
+import com.samourai.whirlpool.client.exception.NotifiableException;
 import com.samourai.whirlpool.client.utils.indexHandler.IIndexHandler;
 import java.lang.invoke.MethodHandles;
 import java.util.List;
@@ -12,17 +13,22 @@ import org.slf4j.LoggerFactory;
 
 public class Bip84ApiWallet extends Bip84Wallet {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final int INIT_BIP84_RETRY = 3;
+  private static final int INIT_BIP84_RETRY_TIMEOUT = 5000;
   private SamouraiApi samouraiApi;
 
   public Bip84ApiWallet(
-      HD_Wallet bip84w, int accountIndex, IIndexHandler indexHandler, SamouraiApi samouraiApi, boolean init)
+      HD_Wallet bip84w,
+      int accountIndex,
+      IIndexHandler indexHandler,
+      SamouraiApi samouraiApi,
+      boolean init)
       throws Exception {
     super(bip84w, accountIndex, indexHandler);
     this.samouraiApi = samouraiApi;
 
     if (init) {
-      log.info(" • Initializing bip84 wallet: "+accountIndex);
-      samouraiApi.initBip84(getZpub());
+      initBip84();
     }
 
     if (indexHandler.get() == 0) {
@@ -47,5 +53,27 @@ public class Bip84ApiWallet extends Bip84Wallet {
       throw new Exception("Address not found");
     }
     return address.account_index;
+  }
+
+  private void initBip84() throws Exception {
+    for (int i = 0; i < INIT_BIP84_RETRY; i++) {
+      log.info(" • Initializing bip84 wallet: " + accountIndex);
+      try {
+        samouraiApi.initBip84(getZpub());
+        return; // success
+      } catch (Exception e) {
+        if (log.isDebugEnabled()) {
+          log.error("", e);
+        }
+        log.error(
+            " x Initializing bip84 wallet failed, retrying... ("
+                + (i + 1)
+                + "/"
+                + INIT_BIP84_RETRY
+                + ")");
+        Thread.sleep(INIT_BIP84_RETRY_TIMEOUT);
+      }
+    }
+    throw new NotifiableException("Unable to initialize Bip84 wallet");
   }
 }
