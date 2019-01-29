561ba67d9f7879b2b9f5c3861144484ed27b675e
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2019-01-29 16:36:53 +0100

    API: add CORS

--- src/main/java/com/samourai/whirlpool/cli/config/CliServicesConfig.java
+++ src/main/java/com/samourai/whirlpool/cli/config/CliServicesConfig.java
@@ -21,6 +21,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableCaching
@@ -87,5 +90,23 @@ public class CliServicesConfig {
         cliConfig.getNetworkParameters(),
         cliConfig.getFee().getXpub(),
         cliConfig.getFee().getValue());
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("OPTIONS");
+    config.addAllowedMethod("HEAD");
+    config.addAllowedMethod("GET");
+    config.addAllowedMethod("PUT");
+    config.addAllowedMethod("POST");
+    config.addAllowedMethod("DELETE");
+    config.addAllowedMethod("PATCH");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
   }
 }
