bcda306e666577d030866328881a6f27f60e000f
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2019-02-14 15:55:41 +0100

    fix tx0Delay

--- src/main/java/com/samourai/whirlpool/cli/services/CliWalletService.java
+++ src/main/java/com/samourai/whirlpool/cli/services/CliWalletService.java
@@ -70,8 +70,8 @@ public class CliWalletService extends WhirlpoolWalletService {
         cliConfig.getMix().getClients(),
         cliConfig.getMix().getClientDelay(),
         cliConfig.getMix().isAutoTx0() ? cliConfig.getMix().getClientDelay() + 5 : 0,
-        cliConfig.getMix().getTx0Delay(),
         cliConfig.getMix().isAutoMix() ? AUTOMIX_DELAY : 0,
+        cliConfig.getMix().getTx0Delay(),
         cliConfig.getMix().getPoolIdsByPriority());
     this.cliConfig = cliConfig;
     this.samouraiApiService = samouraiApiService;
