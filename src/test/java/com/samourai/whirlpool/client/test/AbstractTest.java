0f153b7247891c7d14df1a8ecb28c391e11d83b9
zeroleak <zeroleak@samourai.io>
2019-03-04 20:14:30 +0100

    cleanup

--- src/test/java/com/samourai/whirlpool/client/test/AbstractTest.java
+++ src/test/java/com/samourai/whirlpool/client/test/AbstractTest.java
@@ -1,19 +1,11 @@
 package com.samourai.whirlpool.client.test;
 
-import com.samourai.wallet.hd.java.HD_WalletFactoryJava;
-import com.samourai.wallet.segwit.bech32.Bech32UtilGeneric;
-import org.bitcoinj.core.NetworkParameters;
 import org.junit.After;
 import org.junit.Before;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 
 @SpringBootConfiguration
 public class AbstractTest {
-  @Autowired protected NetworkParameters params;
-  @Autowired protected HD_WalletFactoryJava hdWalletFactory;
-  @Autowired protected Bech32UtilGeneric bech32Util;
-
   @Before
   public void setup() throws Exception {}
 
