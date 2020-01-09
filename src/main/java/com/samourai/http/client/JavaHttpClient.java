973a16cb33a4fb081f663d66161fcc730a691b10
zeroleak <zeroleak@samourai.io>
2020-01-09 19:52:33 +0100

    use JacksonHttpClient

--- src/main/java/com/samourai/http/client/JavaHttpClient.java
+++ src/main/java/com/samourai/http/client/JavaHttpClient.java
@@ -1,17 +1,12 @@
 package com.samourai.http.client;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.samourai.wallet.api.backend.beans.HttpException;
 import com.samourai.whirlpool.cli.config.CliConfig;
 import com.samourai.whirlpool.cli.services.CliTorClientService;
 import com.samourai.whirlpool.cli.utils.CliUtils;
-import io.reactivex.Observable;
 import java.lang.invoke.MethodHandles;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java8.util.Optional;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.ContentResponse;
 import org.eclipse.jetty.client.api.Request;
@@ -24,94 +19,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 
-public class JavaHttpClient implements IHttpClient {
+public class JavaHttpClient extends JacksonHttpClient {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   private CliTorClientService torClientService;
   private CliConfig cliConfig;
-  private ObjectMapper objectMapper;
   private HttpClient httpClientShared;
   private HttpClient httpClientRegOut;
 
   public JavaHttpClient(CliTorClientService torClientService, CliConfig cliConfig) {
+    super();
     this.torClientService = torClientService;
     this.cliConfig = cliConfig;
-    this.objectMapper = new ObjectMapper();
-    objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     httpClientShared = null;
     httpClientRegOut = null;
   }
 
   @Override
-  public synchronized <T> T getJson(
-      String urlStr, Class<T> responseType, Map<String, String> headers) throws HttpException {
-    final boolean isRegOut = false;
-    try {
-      Request req = computeHttpRequest(isRegOut, urlStr, HttpMethod.GET, headers);
-      ContentResponse response = req.send();
-
-      T result = parseResponse(response, responseType);
-      return result;
-    } catch (Exception e) {
-      clearHttpClient(isRegOut);
-      if (log.isDebugEnabled()) {
-        log.error("getJson failed: " + urlStr + ":" + e.getMessage());
-      }
-      if (!(e instanceof HttpException)) {
-        e = new HttpException(e, null);
-      }
-      throw (HttpException) e;
-    }
+  protected String requestJsonGet(String urlStr, Map<String, String> headers) throws Exception {
+    Request req = computeHttpRequest(false, urlStr, HttpMethod.GET, headers);
+    return requestJson(req);
   }
 
   @Override
-  public synchronized <T> Observable<Optional<T>> postJsonOverTor(
-      String urlStr, Class<T> responseType, Map<String, String> headers, Object bodyObj) {
-    return httpObservable(
-        () -> {
-          final boolean isRegOut = true;
-          try {
-            Request request = computeHttpRequest(isRegOut, urlStr, HttpMethod.POST, headers);
-
-            String jsonBody = objectMapper.writeValueAsString(bodyObj);
-            request.content(
-                new StringContentProvider(
-                    MediaType.APPLICATION_JSON_VALUE, jsonBody, StandardCharsets.UTF_8));
-            ContentResponse response = request.send();
-            T result = parseResponse(response, responseType);
-            return result;
-          } catch (Exception e) {
-            clearHttpClient(isRegOut);
-            throw e;
-          }
-        });
+  protected String requestJsonPostOverTor(
+      String urlStr, Map<String, String> headers, String jsonBody) throws Exception {
+    Request req = computeHttpRequest(true, urlStr, HttpMethod.POST, headers);
+    req.content(
+        new StringContentProvider(
+            MediaType.APPLICATION_JSON_VALUE, jsonBody, StandardCharsets.UTF_8));
+    return requestJson(req);
   }
 
   @Override
-  public synchronized <T> T postUrlEncoded(
-      String urlStr, Class<T> responseType, Map<String, String> headers, Map<String, String> body)
-      throws HttpException {
-    final boolean isRegOut = false;
-    try {
-      Request request = computeHttpRequest(isRegOut, urlStr, HttpMethod.POST, headers);
-      if (log.isDebugEnabled()) {
-        log.debug("POST.body=" + body.keySet());
-      }
-      request.content(new FormContentProvider(computeBodyFields(body)));
-      ContentResponse response = request.send();
-
-      T result = parseResponse(response, responseType);
-      return result;
-    } catch (Exception e) {
-      clearHttpClient(isRegOut);
-      if (log.isDebugEnabled()) {
-        log.error("postUrlEncoded failed: " + urlStr, e);
-      }
-      if (!(e instanceof HttpException)) {
-        e = new HttpException(e, null);
-      }
-      throw (HttpException) e;
-    }
+  protected String requestJsonPostUrlEncoded(
+      String urlStr, Map<String, String> headers, Map<String, String> body) throws Exception {
+    Request req = computeHttpRequest(false, urlStr, HttpMethod.POST, headers);
+    req.content(new FormContentProvider(computeBodyFields(body)));
+    return requestJson(req);
   }
 
   private Fields computeBodyFields(Map<String, String> body) {
@@ -122,8 +67,8 @@ public class JavaHttpClient implements IHttpClient {
     return fields;
   }
 
-  private <T> T parseResponse(ContentResponse response, Class<T> responseType) throws Exception {
-    T result = null;
+  private String requestJson(Request req) throws Exception {
+    ContentResponse response = req.send();
     if (response.getStatus() != HttpStatus.OK_200) {
       String responseBody = response.getContentAsString();
       log.error(
@@ -131,13 +76,11 @@ public class JavaHttpClient implements IHttpClient {
       throw new HttpException(
           new Exception("Http query failed: status=" + response.getStatus()), responseBody);
     }
+    String responseContent = response.getContentAsString();
     if (log.isTraceEnabled()) {
-      log.trace("response: " + response.getContentAsString());
+      log.trace("response: " + responseContent);
     }
-    if (responseType != null) {
-      result = objectMapper.readValue(response.getContent(), responseType);
-    }
-    return result;
+    return responseContent;
   }
 
   public HttpClient getHttpClient(boolean isRegisterOutput) throws Exception {
@@ -185,12 +128,13 @@ public class JavaHttpClient implements IHttpClient {
     return httpClient;
   }
 
-  private void clearHttpClient(boolean isRegisterOutput) {
+  @Override
+  protected void onRequestError(Exception e, boolean isRegisterOutput) {
     if (!isRegisterOutput) {
       if (httpClientShared != null) {
         try {
           httpClientShared.stop();
-        } catch (Exception e) {
+        } catch (Exception ee) {
         }
         if (log.isDebugEnabled()) {
           log.debug("--httpClientShared");
@@ -201,7 +145,7 @@ public class JavaHttpClient implements IHttpClient {
       if (httpClientRegOut != null) {
         try {
           httpClientRegOut.stop();
-        } catch (Exception e) {
+        } catch (Exception ee) {
         }
         if (log.isDebugEnabled()) {
           log.debug("--httpClientRegOut");
@@ -209,19 +153,5 @@ public class JavaHttpClient implements IHttpClient {
         httpClientRegOut = null;
       }
     }
-  }
-
-  private <T> Observable<Optional<T>> httpObservable(Callable<T> supplier) {
-    return Observable.fromCallable(
-        () -> {
-          try {
-            return Optional.ofNullable(supplier.call());
-          } catch (Exception e) {
-            if (!(e instanceof HttpException)) {
-              e = new HttpException(e, null);
-            }
-            throw (HttpException) e;
-          }
-        });
   }
 }
